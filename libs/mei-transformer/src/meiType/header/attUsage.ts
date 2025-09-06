7c92d341590d963ec20b60a3a8c8927cc2050ff7
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 01:15:50 +0200

    feat: add AttUsageSchema for documenting attribute usage in MEI
    - Introduced `AttUsageSchema` to define the structure for documenting the usage of specific attributes of elements, including fields for `name`, `context`, and `desc`, in accordance with MEI guidelines.
    - Enhanced type safety and validation for attribute-related data, improving the documentation of attribute usage within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/attUsage.ts
@@ -0,0 +1,37 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { DescSchema } from "../shared/desc";
+
+/**
+ * Documents the usage of a specific attribute of the element.
+ * @see https://music-encoding.org/guidelines/v5/elements/attUsage.html
+ */
+export const AttUsageSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrBiblSchema,
+	Type.Object(
+		{
+			/**
+			 * Name of the attribute.
+			 * @see https://music-encoding.org/guidelines/v5/elements/attUsage.html#name
+			 */
+			name: Type.String(),
+			/**
+			 * Circumstances in which the attribute appears, an XPath expression.
+			 * @see https://music-encoding.org/guidelines/v5/elements/attUsage.html#context
+			 */
+			context: Type.Optional(Type.String()),
+			/**
+			 * Container for text that briefly describes the feature to which it is attached, including its intended usage, purpose, or application as appropriate.
+			 * @see https://music-encoding.org/guidelines/v5/elements/desc.html
+			 */
+			desc: Type.Optional(DescSchema),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type AttUsage = Static<typeof AttUsageSchema>;

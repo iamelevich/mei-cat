1ae24b7ff0baea6931da486dd40c7950bc91b5dc
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 00:49:24 +0200

    feat: update TitleStmtSchema to include detailed optional fields for contributors
    - Enhanced `TitleStmtSchema` by replacing `NotImplementedTagSchema` with specific schemas for `arranger`, `author`, `composer`, `contributor`, `editor`, `funder`, `librettist`, `lyricist`, and `sponsor`, allowing for more comprehensive documentation in accordance with MEI guidelines.
    - Improved type safety and validation for these fields, facilitating better representation of contributors in musical and literary works.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- libs/mei-transformer/src/meiType/header/titleStmt.ts
+++ libs/mei-transformer/src/meiType/header/titleStmt.ts
@@ -1,9 +1,18 @@
 import { type Static, Type } from "@sinclair/typebox";
-import { NotImplementedTagSchema, StandardTagSchema } from "../common";
+import { StandardTagSchema } from "../common";
+import { ArrangerSchema } from "../shared/arranger";
 import { AttrBiblSchema } from "../shared/attr/bibl";
 import { AttrCommonSchema } from "../shared/attr/common";
+import { AuthorSchema } from "../shared/author";
+import { ComposerSchema } from "../shared/composer";
+import { ContributorSchema } from "../shared/contributor";
+import { EditorSchema } from "../shared/editor";
+import { FunderSchema } from "../shared/funder";
 import { HeadSchema } from "../shared/head";
+import { LibrettistSchema } from "../shared/librettist";
+import { LyricistSchema } from "../shared/lyricist";
 import { RespStmtSchema } from "../shared/respStmt";
+import { SponsorSchema } from "../shared/sponsor";
 import { TitleSchema } from "../shared/title";
 
 /**
@@ -37,47 +46,65 @@ export const TitleStmtSchema = Type.Intersect([
 			 * A person or organization who transcribes a musical composition, usually for a different medium from that of the original; in an arrangement the musical substance remains essentially unchanged.
 			 * @see https://music-encoding.org/guidelines/v5/elements/arranger.html
 			 */
-			arranger: Type.Optional(NotImplementedTagSchema),
+			arranger: Type.Optional(
+				Type.Union([ArrangerSchema, Type.Array(ArrangerSchema)]),
+			),
 			/**
 			 * The name of the creator of the intellectual content of a non-musical, literary work.
 			 * @see https://music-encoding.org/guidelines/v5/elements/author.html
 			 */
-			author: Type.Optional(NotImplementedTagSchema),
+			author: Type.Optional(
+				Type.Union([AuthorSchema, Type.Array(AuthorSchema)]),
+			),
 			/**
 			 * The name of the creator of the intellectual content of a musical work.
 			 * @see https://music-encoding.org/guidelines/v5/elements/composer.html
 			 */
-			composer: Type.Optional(NotImplementedTagSchema),
+			composer: Type.Optional(
+				Type.Union([ComposerSchema, Type.Array(ComposerSchema)]),
+			),
 			/**
 			 * Names of individuals, institutions, or organizations responsible for contributions to the intellectual content of a work, where the specialized elements for authors, editors, etc. do not suffice or do not apply.
 			 * @see https://music-encoding.org/guidelines/v5/elements/contributor.html
 			 */
-			contributor: Type.Optional(NotImplementedTagSchema),
+			contributor: Type.Optional(
+				Type.Union([ContributorSchema, Type.Array(ContributorSchema)]),
+			),
 			/**
 			 * The name of the individual(s), institution(s) or organization(s) acting in an editorial capacity.
 			 * @see https://music-encoding.org/guidelines/v5/elements/editor.html
 			 */
-			editor: Type.Optional(NotImplementedTagSchema),
+			editor: Type.Optional(
+				Type.Union([EditorSchema, Type.Array(EditorSchema)]),
+			),
 			/**
 			 * Names of individuals, institutions, or organizations responsible for funding. Funders provide financial support for a project; they are distinct from sponsors, who provide intellectual support and authority.
 			 * @see https://music-encoding.org/guidelines/v5/elements/funder.html
 			 */
-			funder: Type.Optional(NotImplementedTagSchema),
+			funder: Type.Optional(
+				Type.Union([FunderSchema, Type.Array(FunderSchema)]),
+			),
 			/**
 			 * Person or organization who is a writer of the text of an opera, oratorio, etc.
 			 * @see https://music-encoding.org/guidelines/v5/elements/librettist.html
 			 */
-			librettist: Type.Optional(NotImplementedTagSchema),
+			librettist: Type.Optional(
+				Type.Union([LibrettistSchema, Type.Array(LibrettistSchema)]),
+			),
 			/**
 			 * Person or organization who is a writer of the text of a song.
 			 * @see https://music-encoding.org/guidelines/v5/elements/lyricist.html
 			 */
-			lyricist: Type.Optional(NotImplementedTagSchema),
+			lyricist: Type.Optional(
+				Type.Union([LyricistSchema, Type.Array(LyricistSchema)]),
+			),
 			/**
 			 * Names of sponsoring individuals, organizations or institutions. Sponsors give their intellectual authority to a project; they are to be distinguished from funders, who provide the funding but do not necessarily take intellectual responsibility.
 			 * @see https://music-encoding.org/guidelines/v5/elements/sponsor.html
 			 */
-			sponsor: Type.Optional(NotImplementedTagSchema),
+			sponsor: Type.Optional(
+				Type.Union([SponsorSchema, Type.Array(SponsorSchema)]),
+			),
 		},
 		{ additionalProperties: false },
 	),

7e89850ddd988bb9cb3297e6bd2957f25df88544
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 15:27:50 +0200

    feat: add SamplingDeclSchema for encoding sampling declaration in musical works
    - Introduced `SamplingDeclSchema` to define the structure for capturing sampling declaration details, including attributes such as `head` and `p`, in accordance with MEI guidelines.
    - Enhanced type safety and validation for sampling-related data, improving the documentation of musical encoding practices within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/samplingDecl.ts
@@ -0,0 +1,37 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrDataPointingSchema } from "../shared/attr/dataPointing";
+import { AttrLangSchema } from "../shared/attr/lang";
+import { HeadSchema } from "../shared/head";
+import { PSchema } from "../shared/p";
+
+/**
+ * Contains a prose description of the rationale and methods used in sampling texts in the creation of a corpus or collection.
+ * @see https://music-encoding.org/guidelines/v5/elements/samplingDecl.html
+ */
+export const SamplingDeclSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrBiblSchema,
+	AttrDataPointingSchema,
+	AttrLangSchema,
+	Type.Object(
+		{
+			/**
+			 * Contains any heading, for example, the title of a section of text, or the heading of a list.
+			 * @see https://music-encoding.org/guidelines/v5/elements/head.html
+			 */
+			head: Type.Optional(Type.Union([HeadSchema, Type.Array(HeadSchema)])),
+			/**
+			 * One or more text phrases that form a logical prose passage.
+			 * @see https://music-encoding.org/guidelines/v5/elements/p.html
+			 */
+			p: Type.Union([PSchema, Type.Array(PSchema)]),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type SamplingDecl = Static<typeof SamplingDeclSchema>;

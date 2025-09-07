f66fb8efbc55e98279b2383be953700e90a89d87
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 04:03:17 +0200

    feat: add NamespaceSchema for defining namespace elements in musical encoding
    - Introduced `NamespaceSchema` to define the structure for namespace elements, incorporating attributes such as `StandardTagSchema`, `AttrCommonSchema`, and `AttrBiblSchema`, in accordance with MEI guidelines.
    - Enhanced type safety and validation for namespace-related data, improving the documentation of musical encoding practices within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/namespace.ts
@@ -0,0 +1,59 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { DescSchema } from "../shared/desc";
+import { AttUsageSchema } from "./attUsage";
+import { TagUsageSchema } from "./tagUsage";
+
+/**
+ * Supplies the formal name of the namespace to which the elements documented by its children belong.
+ * @see https://music-encoding.org/guidelines/v5/elements/namespace.html
+ */
+export const NamespaceSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrBiblSchema,
+	Type.Object(
+		{
+			/**
+			 * Formal namespace identifier; that is, a uniform resource identifier (URI).
+			 * Value conforms to data.URI.
+			 * @see https://music-encoding.org/guidelines/v5/elements/namespace.html#name
+			 * @see https://music-encoding.org/guidelines/v5/data-types/data.URI.html
+			 */
+			"@name": Type.String(),
+			/**
+			 * Prefix associated with the formal identifier.
+			 * Value conforms to data.NMTOKEN.
+			 * @see https://music-encoding.org/guidelines/v5/elements/namespace.html#prefix
+			 * @see https://music-encoding.org/guidelines/v5/data-types/data.NMTOKEN.html
+			 */
+			"@prefix": Type.Optional(Type.String()),
+			// Optional desc element
+			/**
+			 * Container for text that briefly describes the feature to which it is attached, including its intended usage, purpose, or application as appropriate.
+			 * @see https://music-encoding.org/guidelines/v5/elements/desc.html
+			 */
+			desc: Type.Optional(DescSchema),
+			// Choice: one or more tagUsage OR one or more attUsage
+			/**
+			 * Documents the usage of a specific element within the document.
+			 * @see https://music-encoding.org/guidelines/v5/elements/tagUsage.html
+			 */
+			tagUsage: Type.Optional(
+				Type.Union([TagUsageSchema, Type.Array(TagUsageSchema)]),
+			),
+			/**
+			 * Documents the usage of a specific attribute of the element.
+			 * @see https://music-encoding.org/guidelines/v5/elements/attUsage.html
+			 */
+			attUsage: Type.Optional(
+				Type.Union([AttUsageSchema, Type.Array(AttUsageSchema)]),
+			),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type Namespace = Static<typeof NamespaceSchema>;

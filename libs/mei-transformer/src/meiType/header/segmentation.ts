2a5da846b33f5dc5a0c24a1ee2b32d4a6f3e8b0a
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 15:29:03 +0200

    feat: add SegmentationSchema for encoding musical text segmentation
    - Introduced `SegmentationSchema` to define the structure for capturing principles of musical text segmentation, including attributes such as `head` and `p`, in accordance with MEI guidelines.
    - Enhanced type safety and validation for segmentation-related data, improving the documentation of musical encoding practices within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/segmentation.ts
@@ -0,0 +1,37 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrDataPointingSchema } from "../shared/attr/dataPointing";
+import { AttrLangSchema } from "../shared/attr/lang";
+import { HeadSchema } from "../shared/head";
+import { PSchema } from "../shared/p";
+
+/**
+ * Describes the principles according to which the musical text has been segmented, for example into movements, sections, etc.
+ * @see https://music-encoding.org/guidelines/v5/elements/segmentation.html
+ */
+export const SegmentationSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrBiblSchema,
+	AttrDataPointingSchema,
+	AttrLangSchema,
+	Type.Object(
+		{
+			/**
+			 * Contains any heading, for example, the title of a section of text, or the heading of a list.
+			 * @see https://music-encoding.org/guidelines/v5/elements/head.html
+			 */
+			head: Type.Optional(Type.Union([HeadSchema, Type.Array(HeadSchema)])),
+			/**
+			 * One or more text phrases that form a logical prose passage.
+			 * @see https://music-encoding.org/guidelines/v5/elements/p.html
+			 */
+			p: Type.Union([PSchema, Type.Array(PSchema)]),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type Segmentation = Static<typeof SegmentationSchema>;

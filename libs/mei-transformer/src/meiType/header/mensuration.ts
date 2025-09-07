d4f41c65a6161ae16bf6be9623c4b3015adf496a
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 03:28:52 +0200

    feat: add MensurationSchema for encoding mensuration information in bibliographic descriptions
    - Introduced `MensurationSchema` to define the structure for capturing mensuration details, incorporating attributes such as `StandardTagSchema`, `AttrCommonSchema`, `AttrBiblSchema`, `AttrLangSchema`, `AttrMensurLogSchema`, and `AttrMensurVisSchema`, in accordance with MEI guidelines.
    - Enhanced type safety and validation for mensuration-related data, improving the documentation of musical encoding practices within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/mensuration.ts
@@ -0,0 +1,22 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrLangSchema } from "../shared/attr/lang";
+import { AttrMensurLogSchema } from "../shared/attr/mensurLog";
+import { AttrMensurVisSchema } from "../shared/attr/mensurVis";
+
+/**
+ * Captures information about mensuration within bibliographic descriptions.
+ * @see https://music-encoding.org/guidelines/v5/elements/mensuration.html
+ */
+export const MensurationSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrBiblSchema,
+	AttrLangSchema,
+	AttrMensurLogSchema,
+	AttrMensurVisSchema,
+]);
+
+export type Mensuration = Static<typeof MensurationSchema>;

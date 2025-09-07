f1ea938c4ced61467ba9d45f4e76a4d4677c6ab6
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 03:26:55 +0200

    feat: add KeySchema for encoding tonal center and mode information
    - Introduced `KeySchema` to define the structure for capturing key information, including attributes such as tonal center and mode, in accordance with MEI guidelines.
    - Enhanced type safety and validation for key-related data, improving the documentation of musical encoding practices within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/key.ts
@@ -0,0 +1,22 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrAccidentalSchema } from "../shared/attr/accidental";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrKeyModeSchema } from "../shared/attr/keyMode";
+import { AttrPitchSchema } from "../shared/attr/pitch";
+
+/**
+ * Key captures information about tonal center and mode.
+ * @see https://music-encoding.org/guidelines/v5/elements/key.html
+ */
+export const KeySchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrAccidentalSchema,
+	AttrBiblSchema,
+	AttrKeyModeSchema,
+	AttrPitchSchema,
+]);
+
+export type Key = Static<typeof KeySchema>;

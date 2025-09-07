204041b8d8d8c9a7054e36be0a17f6e741f1219a
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 02:47:39 +0200

    feat: add FoliumSchema for describing individual leaves of paper
    - Introduced `FoliumSchema` to define the structure for a single leaf of paper, incorporating attributes such as `cutout` and `patch`, in accordance with MEI guidelines.
    - Enhanced type safety and validation for folium-related data, improving the documentation of manuscript structures within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/folium.ts
@@ -0,0 +1,37 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrDimensionsSchema } from "../shared/attr/dimensions";
+import { AttrMeasurementSchema } from "../shared/attr/measurement";
+import { AttrFoliumSurfacesSchema } from "./attr/foliumSurfaces";
+import { PatchSchema } from "./patch";
+
+/**
+ * Describes a single leaf of paper.
+ * @see https://music-encoding.org/guidelines/v5/elements/folium.html
+ */
+export const FoliumSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrDimensionsSchema,
+	AttrMeasurementSchema,
+	AttrFoliumSurfacesSchema,
+	Type.Object(
+		{
+			// model.paperModLike elements
+			/**
+			 * A cutout is a section of a document sheet that has been removed and is now missing.
+			 * @see https://music-encoding.org/guidelines/v5/elements/cutout.html
+			 */
+			cutout: Type.Optional(Type.Any()), // TODO: Fix circular dependency with CutoutSchema
+			/**
+			 * Describes a physical writing surface attached to the original document.
+			 * @see https://music-encoding.org/guidelines/v5/elements/patch.html
+			 */
+			patch: Type.Optional(Type.Union([PatchSchema, Type.Array(PatchSchema)])),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type Folium = Static<typeof FoliumSchema>;

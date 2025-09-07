0df41abe8258fb1f645c53a7eb00de1bdb8bf8c0
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-07 15:23:54 +0200

    feat: add ProjectDescSchema for encoding project-level metadata in musical works
    - Introduced `ProjectDescSchema` to define the structure for capturing project-level metadata, including attributes such as `head` and `p`, in accordance with MEI guidelines.
    - Enhanced type safety and validation for project description-related data, improving the documentation of musical encoding practices within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/projectDesc.ts
@@ -0,0 +1,39 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrDataPointingSchema } from "../shared/attr/dataPointing";
+import { AttrLangSchema } from "../shared/attr/lang";
+import { HeadSchema } from "../shared/head";
+import { PSchema } from "../shared/p";
+
+/**
+ * Project-level meta-data describing the aim or purpose for which the electronic file was encoded, funding agencies, etc. together with any other relevant information concerning the process by which it was assembled or collected.
+ * @see https://music-encoding.org/guidelines/v5/elements/projectDesc.html
+ */
+export const ProjectDescSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrBiblSchema,
+	AttrDataPointingSchema,
+	AttrLangSchema,
+	Type.Object(
+		{
+			// Zero or more model.headLike
+			/**
+			 * Contains any heading, for example, the title of a section of text, or the heading of a list.
+			 * @see https://music-encoding.org/guidelines/v5/elements/head.html
+			 */
+			head: Type.Optional(Type.Union([HeadSchema, Type.Array(HeadSchema)])),
+			// One or more model.pLike
+			/**
+			 * One or more text phrases that form a logical prose passage.
+			 * @see https://music-encoding.org/guidelines/v5/elements/p.html
+			 */
+			p: Type.Union([PSchema, Type.Array(PSchema)]),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type ProjectDesc = Static<typeof ProjectDescSchema>;

ed642ad1744811604b1fcf4a960714540706447d
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-14 23:45:31 +0200

    feat: add HeraldrySchema and LocusSchema for enhanced representation in MEI
    - Introduced `HeraldrySchema` to define the structure for heraldic formulas or phrases, incorporating various optional elements for text and editing annotations, in accordance with MEI guidelines.
    - Added `LocusSchema` to represent locations within manuscripts, including folio references and attributes for location specification, enhancing the representation of manuscript data.
    
    These additions improve type safety and support better documentation and validation of complex data structures within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/namesdates/heraldry.ts
@@ -0,0 +1,130 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { DelSchema } from "../edittrans/del";
+import { ExpanSchema } from "../edittrans/expan";
+import { GapSchema } from "../edittrans/gap";
+import { HandShiftSchema } from "../edittrans/handShift";
+import { OrigSchema } from "../edittrans/orig";
+import { RegSchema } from "../edittrans/reg";
+import { RestoreSchema } from "../edittrans/restore";
+import { SicSchema } from "../edittrans/sic";
+import { SubstSchema } from "../edittrans/subst";
+import { SuppliedSchema } from "../edittrans/supplied";
+import { UnclearSchema } from "../edittrans/unclear";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrFacsimileSchema } from "../shared/attr/facsimile";
+import { AttrLangSchema } from "../shared/attr/lang";
+
+/**
+ * Contains a heraldic formula or phrase, typically found as part of a blazon, coat of arms, etc.
+ * @see https://music-encoding.org/guidelines/v5/elements/heraldry.html
+ */
+export const HeraldrySchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrBiblSchema,
+	AttrFacsimileSchema,
+	AttrLangSchema,
+	Type.Object(
+		{
+			// Content model according to MEI spec:
+			// macro.struc-unstrucContent - comprehensive content model including:
+			// - text content
+			// - model.textPhraseLike elements
+			// - model.editLike elements
+			// - model.transcriptionLike elements
+			// - model.msInline elements
+			// - model.physDescPart elements
+			// - and many more
+
+			// model.textPhraseLike elements
+			/**
+			 * A generic element for 1) a shortened form of a word, including an acronym or 2) a shorthand notation.
+			 * @see https://music-encoding.org/guidelines/v5/elements/abbr.html
+			 */
+			abbr: Type.Optional(
+				Type.Union([Type.String(), Type.Array(Type.String())]),
+			),
+
+			// model.editLike elements
+			/**
+			 * Contains information deleted, marked as deleted, or otherwise indicated as superfluous or spurious in the copy text by an author, scribe, annotator, or corrector.
+			 * @see https://music-encoding.org/guidelines/v5/elements/del.html
+			 */
+			del: Type.Optional(Type.Union([DelSchema, Type.Array(DelSchema)])),
+
+			/**
+			 * Contains the expansion of an abbreviation.
+			 * @see https://music-encoding.org/guidelines/v5/elements/expan.html
+			 */
+			expan: Type.Optional(Type.Union([ExpanSchema, Type.Array(ExpanSchema)])),
+
+			/**
+			 * Marks the beginning of a passage written in a new hand, or of a change in the scribe, writing style, ink or character of the document hand.
+			 * @see https://music-encoding.org/guidelines/v5/elements/handShift.html
+			 */
+			handShift: Type.Optional(
+				Type.Union([HandShiftSchema, Type.Array(HandShiftSchema)]),
+			),
+
+			/**
+			 * Contains material which is marked as following the original, rather than being normalized or corrected.
+			 * @see https://music-encoding.org/guidelines/v5/elements/orig.html
+			 */
+			orig: Type.Optional(Type.Union([OrigSchema, Type.Array(OrigSchema)])),
+
+			/**
+			 * Contains material which has been regularized or normalized in some sense.
+			 * @see https://music-encoding.org/guidelines/v5/elements/reg.html
+			 */
+			reg: Type.Optional(Type.Union([RegSchema, Type.Array(RegSchema)])),
+
+			/**
+			 * Indicates restoration of material to an earlier state by cancellation of an editorial or authorial marking or instruction.
+			 * @see https://music-encoding.org/guidelines/v5/elements/restore.html
+			 */
+			restore: Type.Optional(
+				Type.Union([RestoreSchema, Type.Array(RestoreSchema)]),
+			),
+
+			/**
+			 * Contains apparently incorrect or inaccurate material.
+			 * @see https://music-encoding.org/guidelines/v5/elements/sic.html
+			 */
+			sic: Type.Optional(Type.Union([SicSchema, Type.Array(SicSchema)])),
+
+			/**
+			 * Groups transcriptional elements when the combination is to be regarded as a single intervention in the text.
+			 * @see https://music-encoding.org/guidelines/v5/elements/subst.html
+			 */
+			subst: Type.Optional(Type.Union([SubstSchema, Type.Array(SubstSchema)])),
+
+			// model.transcriptionLike elements
+			/**
+			 * Indicates a point where material has been omitted in a transcription, whether as part of sampling practice or for editorial reasons described in the MEI header.
+			 * @see https://music-encoding.org/guidelines/v5/elements/gap.html
+			 */
+			gap: Type.Optional(Type.Union([GapSchema, Type.Array(GapSchema)])),
+
+			/**
+			 * Contains material supplied by the transcriber or editor for any reason.
+			 * @see https://music-encoding.org/guidelines/v5/elements/supplied.html
+			 */
+			supplied: Type.Optional(
+				Type.Union([SuppliedSchema, Type.Array(SuppliedSchema)]),
+			),
+
+			/**
+			 * Contains material that cannot be transcribed with certainty because it is illegible or inaudible in the source.
+			 * @see https://music-encoding.org/guidelines/v5/elements/unclear.html
+			 */
+			unclear: Type.Optional(
+				Type.Union([UnclearSchema, Type.Array(UnclearSchema)]),
+			),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type Heraldry = Static<typeof HeraldrySchema>;
---
+++ libs/mei-transformer/src/meiType/namesdates/locus.ts
@@ -0,0 +1,70 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { NotImplementedTagSchema, StandardTagSchema } from "../common";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrLangSchema } from "../shared/attr/lang";
+import { RendSchema } from "../shared/rend";
+import { SymbolSchema } from "../shared/symbol";
+
+/**
+ * Defines a location within a manuscript or manuscript component, usually as a (possibly discontinuous) sequence of folio references.
+ * @see https://music-encoding.org/guidelines/v5/elements/locus.html
+ */
+export const LocusSchema = Type.Recursive((Self) =>
+	Type.Intersect([
+		StandardTagSchema,
+		AttrCommonSchema,
+		AttrBiblSchema,
+		AttrLangSchema,
+		Type.Object(
+			{
+				// Content model according to MEI spec:
+				// rng:zeroOrMore - choice of text OR locus OR rend OR symbol
+				// Text content is handled by Type.String() in the schema
+
+				// Direct children elements
+				/**
+				 * Defines a location within a manuscript or manuscript component, usually as a (possibly discontinuous) sequence of folio references.
+				 * @see https://music-encoding.org/guidelines/v5/elements/locus.html
+				 */
+				locus: Type.Optional(Type.Union([Self, Type.Array(Self)])),
+
+				/**
+				 * A formatting element indicating special visual rendering, e.g., bold or italicized, of a text word or phrase.
+				 * @see https://music-encoding.org/guidelines/v5/elements/rend.html
+				 */
+				rend: Type.Optional(Type.Union([RendSchema, Type.Array(RendSchema)])),
+
+				/**
+				 * A reference to a previously defined symbol.
+				 * @see https://music-encoding.org/guidelines/v5/elements/symbol.html
+				 */
+				symbol: Type.Optional(
+					Type.Union([SymbolSchema, Type.Array(SymbolSchema)]),
+				),
+
+				// Direct attributes for location specification
+				/**
+				 * Specifies the starting point of the location in a normalized form.
+				 * @see https://music-encoding.org/guidelines/v5/elements/locus.html#from
+				 */
+				from: Type.Optional(Type.String()),
+
+				/**
+				 * Specifies the end-point of the location in a normalized form.
+				 * @see https://music-encoding.org/guidelines/v5/elements/locus.html#to
+				 */
+				to: Type.Optional(Type.String()),
+
+				/**
+				 * Identifies the foliation scheme in terms of which the location is being specified by pointing to some foliation element defining it, or to some other equivalent resource.
+				 * @see https://music-encoding.org/guidelines/v5/elements/locus.html#scheme
+				 */
+				scheme: Type.Optional(Type.String()),
+			},
+			{ additionalProperties: false },
+		),
+	]),
+);
+
+export type Locus = Static<typeof LocusSchema>;

a6c72622e5d5e251d98b5db2dd1f95ac89df0039
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-14 17:29:39 +0200

    feat: add TermListSchema for defining resource descriptions in MEI
    - Introduced `TermListSchema` to define the structure for a collection of text phrases that describe a resource, including optional attributes such as `head`, `label`, and `term`, in accordance with MEI guidelines.
    - Enhanced type safety and validation for resource description-related data, improving the documentation of musical encoding practices within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/header/termList.ts
@@ -0,0 +1,43 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrBiblSchema } from "../shared/attr/bibl";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrPointingSchema } from "../shared/attr/pointing";
+import { HeadSchema } from "../shared/head";
+import { LabelSchema } from "../shared/label";
+import { TermSchema } from "../shared/term";
+
+/**
+ * Collection of text phrases which describe a resource.
+ * @see https://music-encoding.org/guidelines/v5/elements/termList.html
+ */
+export const TermListSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrBiblSchema,
+	AttrPointingSchema,
+	Type.Object(
+		{
+			// Zero or more model.headLike
+			/**
+			 * Contains any heading, for example, the title of a section of text, or the heading of a list.
+			 * @see https://music-encoding.org/guidelines/v5/elements/head.html
+			 */
+			head: Type.Optional(Type.Union([HeadSchema, Type.Array(HeadSchema)])),
+			// Zero or more (optional label, term) pairs
+			/**
+			 * A container for document text that identifies the feature to which it is attached. For a "tool tip" or other generated label, use the label attribute.
+			 * @see https://music-encoding.org/guidelines/v5/elements/label.html
+			 */
+			label: Type.Optional(Type.Union([LabelSchema, Type.Array(LabelSchema)])),
+			/**
+			 * Keyword or phrase which describes a resource.
+			 * @see https://music-encoding.org/guidelines/v5/elements/term.html
+			 */
+			term: Type.Optional(Type.Union([TermSchema, Type.Array(TermSchema)])),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type TermList = Static<typeof TermListSchema>;

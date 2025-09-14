0969cf2b4cf3a24689598b6c3010f9a3ebaf668c
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-14 23:57:43 +0200

    feat: add PeriodNameSchema, PersNameSchema, and PostBoxSchema for enhanced representation in MEI
    - Introduced `PeriodNameSchema` to define the structure for period names, incorporating various optional elements for text and editing annotations, in accordance with MEI guidelines.
    - Added `PersNameSchema` to represent individual names, including components such as forenames and surnames, enhancing the representation of personal data.
    - Introduced `PostBoxSchema` to define postal delivery points, including optional fields for editing elements, improving the representation of address-related data.
    
    These additions enhance type safety and support better documentation and validation of complex data structures within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/namesdates/postBox.ts
@@ -0,0 +1,145 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AddSchema } from "../edittrans/add";
+import { ChoiceSchema } from "../edittrans/choice";
+import { CorrSchema } from "../edittrans/corr";
+import { DamageSchema } from "../edittrans/damage";
+import { DelSchema } from "../edittrans/del";
+import { ExpanSchema } from "../edittrans/expan";
+import { GapSchema } from "../edittrans/gap";
+import { HandShiftSchema } from "../edittrans/handShift";
+import { OrigSchema } from "../edittrans/orig";
+import { RegSchema } from "../edittrans/reg";
+import { RestoreSchema } from "../edittrans/restore";
+import { SicSchema } from "../edittrans/sic";
+import { SubstSchema } from "../edittrans/subst";
+import { SuppliedSchema } from "../edittrans/supplied";
+import { UnclearSchema } from "../edittrans/unclear";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrFacsimileSchema } from "../shared/attr/facsimile";
+import { AttrLangSchema } from "../shared/attr/lang";
+
+/**
+ * Contains a number or other identifier for some postal delivery point other than a street address.
+ * @see https://music-encoding.org/guidelines/v5/elements/postBox.html
+ */
+export const PostBoxSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrFacsimileSchema,
+	AttrLangSchema,
+	Type.Object(
+		{
+			// Content model according to MEI spec:
+			// rng:zeroOrMore - choice of text OR model.editLike OR model.transcriptionLike
+			// Text content is handled by Type.String() in the schema
+
+			// model.editLike elements
+			/**
+			 * Marks an addition to the text.
+			 * @see https://music-encoding.org/guidelines/v5/elements/add.html
+			 */
+			add: Type.Optional(Type.Union([AddSchema, Type.Array(AddSchema)])),
+
+			/**
+			 * Groups a number of alternative encodings for the same point in a text.
+			 * @see https://music-encoding.org/guidelines/v5/elements/choice.html
+			 */
+			choice: Type.Optional(
+				Type.Union([ChoiceSchema, Type.Array(ChoiceSchema)]),
+			),
+
+			/**
+			 * Contains the correct form of an apparent erroneous passage.
+			 * @see https://music-encoding.org/guidelines/v5/elements/corr.html
+			 */
+			corr: Type.Optional(Type.Union([CorrSchema, Type.Array(CorrSchema)])),
+
+			/**
+			 * Contains an area of damage to the physical medium.
+			 * @see https://music-encoding.org/guidelines/v5/elements/damage.html
+			 */
+			damage: Type.Optional(
+				Type.Union([DamageSchema, Type.Array(DamageSchema)]),
+			),
+
+			/**
+			 * Contains information deleted, marked as deleted, or otherwise indicated as superfluous or spurious in the copy text by an author, scribe, annotator, or corrector.
+			 * @see https://music-encoding.org/guidelines/v5/elements/del.html
+			 */
+			del: Type.Optional(Type.Union([DelSchema, Type.Array(DelSchema)])),
+
+			/**
+			 * Contains the expansion of an abbreviation.
+			 * @see https://music-encoding.org/guidelines/v5/elements/expan.html
+			 */
+			expan: Type.Optional(Type.Union([ExpanSchema, Type.Array(ExpanSchema)])),
+
+			/**
+			 * Marks the beginning of a passage written in a new hand, or of a change in the scribe, writing style, ink or character of the document hand.
+			 * @see https://music-encoding.org/guidelines/v5/elements/handShift.html
+			 */
+			handShift: Type.Optional(
+				Type.Union([HandShiftSchema, Type.Array(HandShiftSchema)]),
+			),
+
+			/**
+			 * Contains material which is marked as following the original, rather than being normalized or corrected.
+			 * @see https://music-encoding.org/guidelines/v5/elements/orig.html
+			 */
+			orig: Type.Optional(Type.Union([OrigSchema, Type.Array(OrigSchema)])),
+
+			/**
+			 * Contains material which has been regularized or normalized in some sense.
+			 * @see https://music-encoding.org/guidelines/v5/elements/reg.html
+			 */
+			reg: Type.Optional(Type.Union([RegSchema, Type.Array(RegSchema)])),
+
+			/**
+			 * Indicates restoration of material to an earlier state by cancellation of an editorial or authorial marking or instruction.
+			 * @see https://music-encoding.org/guidelines/v5/elements/restore.html
+			 */
+			restore: Type.Optional(
+				Type.Union([RestoreSchema, Type.Array(RestoreSchema)]),
+			),
+
+			/**
+			 * Contains apparently incorrect or inaccurate material.
+			 * @see https://music-encoding.org/guidelines/v5/elements/sic.html
+			 */
+			sic: Type.Optional(Type.Union([SicSchema, Type.Array(SicSchema)])),
+
+			/**
+			 * Groups transcriptional elements when the combination is to be regarded as a single intervention in the text.
+			 * @see https://music-encoding.org/guidelines/v5/elements/subst.html
+			 */
+			subst: Type.Optional(Type.Union([SubstSchema, Type.Array(SubstSchema)])),
+
+			// model.transcriptionLike elements
+			/**
+			 * Indicates a point where material has been omitted in a transcription, whether as part of sampling practice or for editorial reasons described in the MEI header.
+			 * @see https://music-encoding.org/guidelines/v5/elements/gap.html
+			 */
+			gap: Type.Optional(Type.Union([GapSchema, Type.Array(GapSchema)])),
+
+			/**
+			 * Contains material supplied by the transcriber or editor for any reason.
+			 * @see https://music-encoding.org/guidelines/v5/elements/supplied.html
+			 */
+			supplied: Type.Optional(
+				Type.Union([SuppliedSchema, Type.Array(SuppliedSchema)]),
+			),
+
+			/**
+			 * Contains material that cannot be transcribed with certainty because it is illegible or inaudible in the source.
+			 * @see https://music-encoding.org/guidelines/v5/elements/unclear.html
+			 */
+			unclear: Type.Optional(
+				Type.Union([UnclearSchema, Type.Array(UnclearSchema)]),
+			),
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type PostBox = Static<typeof PostBoxSchema>;

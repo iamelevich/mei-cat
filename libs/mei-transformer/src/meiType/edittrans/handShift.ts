2951b17074e1eb87f5fed34ee888cb646b897395
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-14 19:55:26 +0200

    feat: add HandShiftSchema for representing changes in handwriting in MEI
    - Introduced `HandShiftSchema` to define the structure for marking the beginning of a passage written in a new hand or changes in the scribe, writing style, ink, or character of the document hand, in accordance with MEI guidelines.
    - The schema includes optional fields for attributes such as `@character`, `@new`, and `@old`, enhancing the documentation and validation of handwriting-related data within the MEI framework.
    
    This addition improves type safety and supports better representation of musical encoding practices in MEI.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

---
+++ libs/mei-transformer/src/meiType/edittrans/handShift.ts
@@ -0,0 +1,50 @@
+import { type Static, Type } from "@sinclair/typebox";
+import { StandardTagSchema } from "../common";
+import { AttrCommonSchema } from "../shared/attr/common";
+import { AttrFacsimileSchema } from "../shared/attr/facsimile";
+import { AttrMediumSchema } from "../shared/attr/medium";
+import { AttrEditSchema } from "./attr/edit";
+
+/**
+ * Marks the beginning of a passage written in a new hand, or of a change in the scribe, writing style, ink or character of the document hand.
+ * @see https://music-encoding.org/guidelines/v5/elements/handShift.html
+ */
+export const HandShiftSchema = Type.Intersect([
+	StandardTagSchema,
+	AttrCommonSchema,
+	AttrEditSchema,
+	AttrFacsimileSchema,
+	AttrMediumSchema,
+	Type.Object(
+		{
+			/**
+			 * Describes the character of the new hand.
+			 * Value is plain text.
+			 * @see https://music-encoding.org/guidelines/v5/elements/handShift.html#character
+			 */
+			"@character": Type.Optional(Type.String()),
+
+			/**
+			 * Identifies the new hand.
+			 * The value must contain the ID of a hand element given elsewhere in the document.
+			 * Value conforms to data.URI.
+			 * @see https://music-encoding.org/guidelines/v5/elements/handShift.html#new
+			 */
+			"@new": Type.Optional(Type.String()),
+
+			/**
+			 * Identifies the old hand.
+			 * The value must contain the ID of a hand element given elsewhere in the document.
+			 * Value conforms to data.URI.
+			 * @see https://music-encoding.org/guidelines/v5/elements/handShift.html#old
+			 */
+			"@old": Type.Optional(Type.String()),
+
+			// Content model: empty
+			// The handShift element may not have child elements according to MEI specification
+		},
+		{ additionalProperties: false },
+	),
+]);
+
+export type HandShift = Static<typeof HandShiftSchema>;

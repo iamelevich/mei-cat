163c29be436df715df307afd42302262f3891ae9
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-17 21:54:30 +0200

    refactor: streamline SylSchema imports and enhance modularity
    - Removed unused imports from the `syl.ts` file, focusing on essential schemas to improve clarity.
    - Updated optional element definitions to utilize `Type.Ref`, ensuring consistency and maintainability across the MEI framework.
    
    These changes contribute to a more organized and efficient codebase within the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- libs/mei-transformer/src/meiType/text/syl.ts
+++ libs/mei-transformer/src/meiType/text/syl.ts
@@ -1,35 +1,9 @@
-import { type Static, Type } from "typebox";
+import { Type } from "typebox";
 import { StandardTagSchema } from "../common";
-import { AddSchema } from "../edittrans/add";
-import { AppSchema } from "../edittrans/app";
-import { ChoiceSchema } from "../edittrans/choice";
-import { CorrSchema } from "../edittrans/corr";
-import { DamageSchema } from "../edittrans/damage";
-import { DelSchema } from "../edittrans/del";
-import { ExpanSchema } from "../edittrans/expan";
-import { GapSchema } from "../edittrans/gap";
-import { HandShiftSchema } from "../edittrans/handShift";
-import { LemSchema } from "../edittrans/lem";
-import { OrigSchema } from "../edittrans/orig";
-import { RdgSchema } from "../edittrans/rdg";
-import { RegSchema } from "../edittrans/reg";
-import { RestoreSchema } from "../edittrans/restore";
-import { SicSchema } from "../edittrans/sic";
-import { SubstSchema } from "../edittrans/subst";
-import { SuppliedSchema } from "../edittrans/supplied";
-import { UnclearSchema } from "../edittrans/unclear";
-import { AbbrSchema } from "../shared/abbr";
-import { AnnotSchema } from "../shared/annot";
 import { AttrCommonSchema } from "../shared/attr/common";
 import { AttrFacsimileSchema } from "../shared/attr/facsimile";
 import { AttrLangSchema } from "../shared/attr/lang";
 import { AttrTextRenditionSchema } from "../shared/attr/textRendition";
-import { LbSchema } from "../shared/lb";
-import { NameSchema } from "../shared/name";
-import { NumSchema } from "../shared/num";
-import { PbSchema } from "../shared/pb";
-import { RendSchema } from "../shared/rend";
-import { TermSchema } from "../shared/term";
 
 /**
  * Individual lyric syllable.
@@ -52,7 +26,9 @@ export const SylSchema = Type.Intersect([
 			 * Marks an addition to the text.
 			 * @see https://music-encoding.org/guidelines/v5/elements/add.html
 			 */
-			add: Type.Optional(Type.Union([Type.Ref("add"), Type.Array(Type.Ref("add"))])),
+			add: Type.Optional(
+				Type.Union([Type.Ref("add"), Type.Array(Type.Ref("add"))]),
+			),
 
 			/**
 			 * Groups a number of alternative encodings for the same point in a text.
@@ -66,7 +42,9 @@ export const SylSchema = Type.Intersect([
 			 * Contains the correct form of an apparent erroneous passage.
 			 * @see https://music-encoding.org/guidelines/v5/elements/corr.html
 			 */
-			corr: Type.Optional(Type.Union([Type.Ref("corr"), Type.Array(Type.Ref("corr"))])),
+			corr: Type.Optional(
+				Type.Union([Type.Ref("corr"), Type.Array(Type.Ref("corr"))]),
+			),
 
 			/**
 			 * Contains an area of damage to the physical medium.
@@ -80,13 +58,17 @@ export const SylSchema = Type.Intersect([
 			 * Contains information deleted, marked as deleted, or otherwise indicated as superfluous or spurious in the copy text by an author, scribe, annotator, or corrector.
 			 * @see https://music-encoding.org/guidelines/v5/elements/del.html
 			 */
-			del: Type.Optional(Type.Union([Type.Ref("del"), Type.Array(Type.Ref("del"))])),
+			del: Type.Optional(
+				Type.Union([Type.Ref("del"), Type.Array(Type.Ref("del"))]),
+			),
 
 			/**
 			 * Contains the expansion of an abbreviation.
 			 * @see https://music-encoding.org/guidelines/v5/elements/expan.html
 			 */
-			expan: Type.Optional(Type.Union([Type.Ref("expan"), Type.Array(Type.Ref("expan"))])),
+			expan: Type.Optional(
+				Type.Union([Type.Ref("expan"), Type.Array(Type.Ref("expan"))]),
+			),
 
 			/**
 			 * Marks the beginning of a passage written in a new hand, or of a change in the scribe, writing style, ink or character of the document hand.
@@ -100,96 +82,122 @@ export const SylSchema = Type.Intersect([
 			 * Contains material which is marked as following the original, rather than being normalized or corrected.
 			 * @see https://music-encoding.org/guidelines/v5/elements/orig.html
 			 */
-			orig: Type.Optional(Type.Union([Type.Ref("orig"), Type.Array(Type.Ref("orig"))])),
+			orig: Type.Optional(
+				Type.Union([Type.Ref("orig"), Type.Array(Type.Ref("orig"))]),
+			),
 
 			/**
 			 * Contains material which has been regularized or normalized in some sense.
 			 * @see https://music-encoding.org/guidelines/v5/elements/reg.html
 			 */
-			reg: Type.Optional(Type.Union([Type.Ref("reg"), Type.Array(Type.Ref("reg"))])),
+			reg: Type.Optional(
+				Type.Union([Type.Ref("reg"), Type.Array(Type.Ref("reg"))]),
+			),
 
 			/**
 			 * Indicates restoration of material to an earlier state by cancellation of an editorial or authorial marking or instruction.
 			 * @see https://music-encoding.org/guidelines/v5/elements/restore.html
 			 */
 			restore: Type.Optional(
-				Type.Union([RestoreSchema, Type.Array(RestoreSchema)]),
+				Type.Union([Type.Ref("restore"), Type.Array(Type.Ref("restore"))]),
 			),
 
 			/**
 			 * Contains apparently incorrect or inaccurate material.
 			 * @see https://music-encoding.org/guidelines/v5/elements/sic.html
 			 */
-			sic: Type.Optional(Type.Union([SicSchema, Type.Array(SicSchema)])),
+			sic: Type.Optional(
+				Type.Union([Type.Ref("sic"), Type.Array(Type.Ref("sic"))]),
+			),
 
 			/**
 			 * Groups transcriptional elements when the combination is to be regarded as a single intervention in the text.
 			 * @see https://music-encoding.org/guidelines/v5/elements/subst.html
 			 */
-			subst: Type.Optional(Type.Union([SubstSchema, Type.Array(SubstSchema)])),
+			subst: Type.Optional(
+				Type.Union([Type.Ref("subst"), Type.Array(Type.Ref("subst"))]),
+			),
 
 			// model.textPhraseLike.limited elements
 			/**
 			 * A generic element for 1) a shortened form of a word, including an acronym or 2) a shorthand notation.
 			 * @see https://music-encoding.org/guidelines/v5/elements/abbr.html
 			 */
-			abbr: Type.Optional(Type.Union([AbbrSchema, Type.Array(AbbrSchema)])),
+			abbr: Type.Optional(
+				Type.Union([Type.Ref("abbr"), Type.Array(Type.Ref("abbr"))]),
+			),
 
 			/**
 			 * Provides a statement explaining the text or indicating the basis for an assertion.
 			 * @see https://music-encoding.org/guidelines/v5/elements/annot.html
 			 */
-			annot: Type.Optional(Type.Union([AnnotSchema, Type.Array(AnnotSchema)])),
+			annot: Type.Optional(
+				Type.Union([Type.Ref("annot"), Type.Array(Type.Ref("annot"))]),
+			),
 
 			/**
 			 * An empty formatting element that forces text to begin on a new line.
 			 * @see https://music-encoding.org/guidelines/v5/elements/lb.html
 			 */
-			lb: Type.Optional(Type.Union([LbSchema, Type.Array(LbSchema)])),
+			lb: Type.Optional(
+				Type.Union([Type.Ref("lb"), Type.Array(Type.Ref("lb"))]),
+			),
 
 			/**
 			 * Proper noun or noun phrase.
 			 * @see https://music-encoding.org/guidelines/v5/elements/name.html
 			 */
-			name: Type.Optional(Type.Union([NameSchema, Type.Array(NameSchema)])),
+			name: Type.Optional(
+				Type.Union([Type.Ref("name"), Type.Array(Type.Ref("name"))]),
+			),
 
 			/**
 			 * Numeric information in any form.
 			 * @see https://music-encoding.org/guidelines/v5/elements/num.html
 			 */
-			num: Type.Optional(Type.Union([NumSchema, Type.Array(NumSchema)])),
+			num: Type.Optional(
+				Type.Union([Type.Ref("num"), Type.Array(Type.Ref("num"))]),
+			),
 
 			/**
 			 * An empty formatting element that forces text to begin on a new page.
 			 * @see https://music-encoding.org/guidelines/v5/elements/pb.html
 			 */
-			pb: Type.Optional(Type.Union([PbSchema, Type.Array(PbSchema)])),
+			pb: Type.Optional(
+				Type.Union([Type.Ref("pb"), Type.Array(Type.Ref("pb"))]),
+			),
 
 			/**
 			 * A formatting element indicating special visual rendering, e.g., bold or italicized, of a text word or phrase.
 			 * @see https://music-encoding.org/guidelines/v5/elements/rend.html
 			 */
-			rend: Type.Optional(Type.Union([RendSchema, Type.Array(RendSchema)])),
+			rend: Type.Optional(
+				Type.Union([Type.Ref("rend"), Type.Array(Type.Ref("rend"))]),
+			),
 
 			/**
 			 * Keyword or phrase which describes a resource.
 			 * @see https://music-encoding.org/guidelines/v5/elements/term.html
 			 */
-			term: Type.Optional(Type.Union([TermSchema, Type.Array(TermSchema)])),
+			term: Type.Optional(
+				Type.Union([Type.Ref("term"), Type.Array(Type.Ref("term"))]),
+			),
 
 			// model.transcriptionLike elements
 			/**
 			 * Indicates a point where material has been omitted in a transcription, whether as part of sampling practice or for editorial reasons described in the MEI header.
 			 * @see https://music-encoding.org/guidelines/v5/elements/gap.html
 			 */
-			gap: Type.Optional(Type.Union([GapSchema, Type.Array(GapSchema)])),
+			gap: Type.Optional(
+				Type.Union([Type.Ref("gap"), Type.Array(Type.Ref("gap"))]),
+			),
 
 			/**
 			 * Contains material supplied by the transcriber or editor for any reason.
 			 * @see https://music-encoding.org/guidelines/v5/elements/supplied.html
 			 */
 			supplied: Type.Optional(
-				Type.Union([SuppliedSchema, Type.Array(SuppliedSchema)]),
+				Type.Union([Type.Ref("supplied"), Type.Array(Type.Ref("supplied"))]),
 			),
 
 			/**
@@ -197,7 +205,7 @@ export const SylSchema = Type.Intersect([
 			 * @see https://music-encoding.org/guidelines/v5/elements/unclear.html
 			 */
 			unclear: Type.Optional(
-				Type.Union([UnclearSchema, Type.Array(UnclearSchema)]),
+				Type.Union([Type.Ref("unclear"), Type.Array(Type.Ref("unclear"))]),
 			),
 
 			// model.appLike elements
@@ -205,22 +213,26 @@ export const SylSchema = Type.Intersect([
 			 * Contains one or more alternative encodings for the same point in a text.
 			 * @see https://music-encoding.org/guidelines/v5/elements/app.html
 			 */
-			app: Type.Optional(Type.Union([AppSchema, Type.Array(AppSchema)])),
+			app: Type.Optional(
+				Type.Union([Type.Ref("app"), Type.Array(Type.Ref("app"))]),
+			),
 
 			/**
 			 * Contains the lemma, or base text, of a textual variation.
 			 * @see https://music-encoding.org/guidelines/v5/elements/lem.html
 			 */
-			lem: Type.Optional(Type.Union([LemSchema, Type.Array(LemSchema)])),
+			lem: Type.Optional(
+				Type.Union([Type.Ref("lem"), Type.Array(Type.Ref("lem"))]),
+			),
 
 			/**
 			 * Contains a single reading within a textual variation.
 			 * @see https://music-encoding.org/guidelines/v5/elements/rdg.html
 			 */
-			rdg: Type.Optional(Type.Union([RdgSchema, Type.Array(RdgSchema)])),
+			rdg: Type.Optional(
+				Type.Union([Type.Ref("rdg"), Type.Array(Type.Ref("rdg"))]),
+			),
 		},
 		{ additionalProperties: false },
 	),
 ]);
-
-export type Syl = Static<typeof SylSchema>;

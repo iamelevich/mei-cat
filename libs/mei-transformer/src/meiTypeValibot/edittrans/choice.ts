ff3eaa44e379742a792f7217582f37386fef0291
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-18 21:32:49 +0200

    feat: enhance choice and corr schemas for editorial transcription
    - Updated `ChoiceSchema` to use `v.intersect()` for better structure, incorporating `ChoicePartSchema` and relevant attributes, improving compliance with MEI specifications.
    - Refined `CorrSchema` to include additional attributes and child elements, enhancing its representation of corrections in editorial transcription.
    - Improved import organization for clarity and modularity within the framework.
    
    These changes contribute to a more robust representation of editorial transcription elements in the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- libs/mei-transformer/src/meiTypeValibot/edittrans/choice.ts
+++ libs/mei-transformer/src/meiTypeValibot/edittrans/choice.ts
@@ -1,16 +1,18 @@
 import * as v from "valibot";
 import { StandardTagSchema } from "../common";
-import { AttrCommonSchema } from "../shared/attr/common";
-import { AttrLangSchema } from "../shared/attr/lang";
+import { AttrCommonSchema } from "../shared";
+import { ChoicePartSchema } from "./model/choicePart";
 
 /**
- * Simplified schema for choice element.
+ * Groups a number of alternative encodings for the same point in a text.
  * @see https://music-encoding.org/guidelines/v5/elements/choice.html
  */
-export const ChoiceSchema = v.object({
-	...StandardTagSchema.entries,
-	...AttrCommonSchema.entries,
-	...AttrLangSchema.entries,
-});
+export const ChoiceSchema = v.intersect([
+	v.object({
+		...StandardTagSchema.entries,
+		...AttrCommonSchema.entries,
+	}),
+	ChoicePartSchema,
+]);
 
 export type ChoiceData = v.InferOutput<typeof ChoiceSchema>;

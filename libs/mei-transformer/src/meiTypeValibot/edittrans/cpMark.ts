30173ea5bc74f7fdc5ddb5b09d0fb7958711bc46
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-18 21:39:58 +0200

    feat: implement CpMarkSchema for editorial transcription
    - Developed `CpMarkSchema` using `v.intersect()` to combine relevant attributes and models, enhancing its structure and compliance with MEI specifications.
    - Included attributes from `AttrCpMarkLogSchema`, `AttrCpMarkVisSchema`, `AttrCpMarkGesSchema`, and `AttrCpMarkAnlSchema`, along with `EditLikeSchema`, `TextPhraseLikeLimitedSchema`, and `TranscriptionLikeSchema` for comprehensive representation.
    - Improved import organization for clarity and modularity within the framework.
    
    These changes contribute to a more robust representation of copyist marks in the MEI framework.
    
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- libs/mei-transformer/src/meiTypeValibot/edittrans/cpMark.ts
+++ libs/mei-transformer/src/meiTypeValibot/edittrans/cpMark.ts
@@ -1,10 +1,35 @@
 import * as v from "valibot";
-import { AnythingObjectSchema } from "../common";
+import { AttrCpMarkAnlSchema } from "../analytical/attr/cpMark.anl";
+import { StandardTagSchema } from "../common";
+import { AttrFacsimileSchema } from "../facsimile";
+import { AttrCpMarkGesSchema } from "../gestural/attr/cpMark.ges";
+import {
+	AttrCommonSchema,
+	AttrLangSchema,
+	TextPhraseLikeLimitedSchema,
+} from "../shared";
+import { AttrCpMarkVisSchema } from "../visual/attr/cpMark.vis";
+import { AttrCpMarkLogSchema } from "./attr/cpMark.log";
+import { EditLikeSchema, TranscriptionLikeSchema } from "./model";
 
 /**
- * Indicates a copyist's mark.
+ * A verbal or graphical indication to copy musical material written elsewhere.
  * @see https://music-encoding.org/guidelines/v5/elements/cpMark.html
  */
-export const CpMarkSchema = AnythingObjectSchema; // TODO: implement element
+export const CpMarkSchema = v.intersect([
+	v.object({
+		...StandardTagSchema.entries,
+		...AttrCommonSchema.entries,
+		...AttrFacsimileSchema.entries,
+		...AttrLangSchema.entries,
+		...AttrCpMarkLogSchema.entries,
+		...AttrCpMarkVisSchema.entries,
+		...AttrCpMarkGesSchema.entries,
+		...AttrCpMarkAnlSchema.entries,
+	}),
+	EditLikeSchema,
+	TextPhraseLikeLimitedSchema,
+	TranscriptionLikeSchema,
+]);
 
 export type CpMarkData = v.InferOutput<typeof CpMarkSchema>;

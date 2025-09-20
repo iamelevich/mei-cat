815e9491438549d1696bbd69abaeda87c1a0a254
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-09-20 21:35:43 +0200

    feat: use react-router for client
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,10 @@
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 	server: {
 		proxy: {
 			"/api": {
